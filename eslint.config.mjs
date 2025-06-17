d336eae3634c84ec6fe420cc57ffde97462b3588
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-06-17 10:25:28 +0800

    chore: add eslint perfectionist plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import perfectionist from "eslint-plugin-perfectionist";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,6 +10,16 @@ const compat = new FlatCompat({
     baseDirectory: __dirname
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript", "prettier")];
+const eslintConfig = [
+    ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+    {
+        plugins: {
+            perfectionist
+        },
+        rules: {
+            "perfectionist/sort-imports": "error"
+        }
+    }
+];
 
 export default eslintConfig;
