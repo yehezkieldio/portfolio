270fee0aca9843f8848c3ecd62e982e28ba7b37c
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-10-26 13:40:31 +0800

    feat!: wipe old project

--- src/lib/socials.ts
+++
@@ -1,6 +0,0 @@
-export const socials = {
-    github: "https://github.com/yehezkieldio",
-    x: "https://x.com/yhezkieldio",
-    instagram: "https://www.instagram.com/yhezkiel.dio",
-    linkedin: "https://www.linkedin.com/in/yehezkieldio"
-};
