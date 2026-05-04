4d55ea8720251847bedbfd35fb4553a34c048c29
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2026-05-04 11:27:21 +0800

    style(app): remove unused theme variables from globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ export default function Home() {
     return (
         <div>
             <h1>Hello, world!</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque voluptate quo earum atque repellendus
+                ducimus enim eos obcaecati!
+            </p>
         </div>
     );
 }
